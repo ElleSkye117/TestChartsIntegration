4e5466e5dab236699f02d60659fe1af8b5d9a517
Michael.Leber <michael.leber@nutrien.com>
2018-09-26 09:33:12 -0600

    commit core project code

---
+++ ChartApp/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  ChartApp
+//
+//  Created by MLeber on 9/25/18.
+//  Copyright © 2018 MLeber. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DataProvider {
+    static var data = [CGPoint(x: 0.0, y: 0.0), CGPoint(x: 5.0, y: 10.0), CGPoint(x: 10.0, y: 20.0)]
+}
